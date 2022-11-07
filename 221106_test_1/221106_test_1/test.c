90e81de06da7a78a204580790eddaa1fa13394bd
lxybit <2166318849@qq.com>
2022-11-07 23:09:44 +0800

    daily exercise

---
+++ 221106_test_1/221106_test_1/test.c
@@ -0,0 +1,12 @@
+# define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+int main()
+{
+	printf("%x\n", 111);
+	printf("%X\n", 111);
+	printf("%#x\n", 111);
+	printf("%#X\n", 111);
+
+	return 0;
+}
