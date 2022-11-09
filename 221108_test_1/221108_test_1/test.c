4117d9cf545259b87253add95ebcbe6a39deffc6
lxybit <2166318849@qq.com>
2022-11-09 23:08:13 +0800

    daily exercise

--- 221108_test_1/221108_test_1/test.c
+++ 221108_test_1/221108_test_1/test.c
@@ -1,17 +1,27 @@
 # define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-int main()
-{
-    int a = 1;//0x00 00 00 01
-    char* p = (char*)&a;//int* 解引用一口气访问四个字节
-    if (*p == '1')
-    {
-        printf("小端\n");
-    }
-    else
-    {
-        printf("大端\n");
-    }
+//int main()
+//{
+//    int a = 1;//0x00 00 00 01
+//    char* p = (char*)&a;//int* 解引用一口气访问四个字节
+//    if (*p == '1')
+//    {
+//        printf("小端\n");
+//    }
+//    else
+//    {
+//        printf("大端\n");
+//    }
+//
+//    return 0;
+//}
 
-    return 0;
-}
+//int main()
+//{
+//	char a = 128;
+//
+//	printf("%d\n", a);
+//	printf("%u\n", a);
+//
+//	return 0;
+//}
